c42105a715cfe9c48cbeb7402566cbb2c5a2f59f
sumit meghrajani <sumit.disolutions@gmail.com>
2024-10-29 18:14:09 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 // vite.config.js
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repo-name/', // replace 'your-repo-name' with your GitHub repository name
+  server: {
+    hmr: {
+      overlay: false,
+    },
+  },
 });
